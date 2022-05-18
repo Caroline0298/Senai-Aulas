e31eeeff56165b608735e8b0f2a37d6a6b8e36c9
Carol Figueira <Carol Figueira@HOME_PC>
2022-05-18 13:11:04 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+       
+             static void Main( string[] args)
+            {
+            DateTime d1 = DateTime.Now;
+            Console.WriteLine(d1);
+
+
+            }
+        
+    }
+
+}
